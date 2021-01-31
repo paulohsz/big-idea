d532464494abcd7f8a92972a066a7023c07fe377
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-01-31 03:11:48 +0000

    Rename Folder Header2 -> Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './component/home/Header2';
+import Header from './component/home/Header';
 import Tip from './component/home/Tip';
 import RegisterNewsletter from './component/home/RegisterNewsletter';
 import Footer from './component/home/Footer';
